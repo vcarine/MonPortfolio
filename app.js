638c7e8b6cda934d4f60a47c9c4aa4ec9c605e55
Carine <vinagre.carine@gmail.com>
2022-01-31 11:13:40 +0100

    js ne fonctionne

--- app.js
+++ app.js
@@ -16,7 +16,20 @@ if (window.matchMedia('(max-width)')){
         })
     })
 }
+//Animation de L'écriture
 
+const bienvenue = document.querySelector('.bienvenue');
+
+let typewriter1 = new Typewriter(bienvenue,{
+    loop: false,
+    deleteSpeed: 20
+})
+typewriter1
+    .typeString('<span style="color: black ">Bienvenue sur mon Portfolio</span> ')
+    .pauseFor(1800)
+    .changeDelay(20)
+
+    .start()
 
 //Animation de L'écriture
 
@@ -26,8 +39,23 @@ let typewriter = new Typewriter(txtAnim,{
     loop: false,
     deleteSpeed:20
 })
+
 typewriter
     .pauseFor(1800)
     .changeDelay(20)
-    .typeString('Salut')
+    .typeString('Carine Vinagre')
+    .pauseFor(300)
+    .typeString('<strong>, Développeur Full-Stack </strong>!')
+    .pauseFor(1000)
+    .deleteChars(13)
+    .typeString('<span style="color: #27ae60;"> Html, Css, Bootstrap, Js</span> ')
+    .pauseFor(1000)
+    .deleteChars(22)
+    .typeString('<span style="color: #EA39ff;"> Php, Symfony</span> ')
+    .pauseFor(1000)
+    .deleteChars(14)
+    .typeString('<span style="color: midnightblue;"> Angular</span> ')
+    .pauseFor(1000)
+    .deleteChars(8)
+    .typeString('<span style="color: midnightblue;"> Wordpress</span> !')
     .start()
